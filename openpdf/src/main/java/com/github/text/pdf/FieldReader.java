f37ff16ad231c9d737b8180cf330defa10f16f7d
schmiddc <dan.schmidt1@gmail.com>
2017-12-15 15:23:56 -0500

    first commit

---
+++ openpdf/src/main/java/com/github/text/pdf/FieldReader.java
@@ -0,0 +1,17 @@
+package com.github.text.pdf;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Represents the basic needs for reading fields.
+ */
+public interface FieldReader {
+
+    HashMap getFields();
+
+    String getFieldValue(String fieldKey);
+
+    List getListValues(String fieldKey);
+
+}
